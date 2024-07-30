2ce02f4173777c910018f6a1b23a4b3b352e28ff
PRIYANSHU SINGH <92785258+Priyanshusingh25@users.noreply.github.com>
2024-07-30 12:26:21 +0000

    JS files

---
+++ JavaScript/variables.js
@@ -0,0 +1,13 @@
+const accountId = 12322
+ // can't change the value
+let accountEmail = "priyanshu@google.com"
+// we change the values
+var accountPassword ="12345"
+// {} <--this is called SCOPE in js
+accountCity = "Pune"
+accountEmail="yash@hu.com"
+accountCity="dubai"
+
+// accountId = 2 not allowed
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
