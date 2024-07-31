f438c2a60b7bd35dd04db46b13751f7be5e02df4
PRIYANSHU SINGH <92785258+Priyanshusingh25@users.noreply.github.com>
2024-07-31 05:53:00 +0000

    Conversion of datatypes

---
+++ JavaScript/conversionsandoperations.js
@@ -0,0 +1,22 @@
+//let score ="priyanshu" //if null, then 0 ; if true,then 1
+
+//const {score} = req.body
+//console.log(typeof score)
+//console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); // shows NaN if score is not an actual number
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
