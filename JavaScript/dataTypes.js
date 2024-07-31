f2d74bdbe3359a3e692107b3b8bf98cfde15bef8
PRIYANSHU SINGH <92785258+Priyanshusingh25@users.noreply.github.com>
2024-07-31 07:03:45 +0000

    datatypes

--- JavaScript/dataTypes.js
+++ JavaScript/dataTypes.js
@@ -9,9 +9,9 @@ let age="18"
 let isLoggedIn= false
 let state = null // shows emptiness , type is object
 
-          // primitive datatypes
+          // primitive datatypes (is Call by value)
 // number => 2 to the power 53
-//bigint
+//bigInt
 //string => ""
 //boolean => true or false
 //null -- standalone value type
@@ -19,3 +19,39 @@ let state = null // shows emptiness , type is object
 //symbol -- for uniqueness
 
 console.log(typeof age);
+
+
+//------------------non-primitive (reference type)---------------------//
+
+// Array
+// Objects
+// Functions
+
+// const age= 100
+// const minAge = 90
+// let isLoggedIn = false
+const outsideTemp = null
+// let userEmail;
+
+const id = Symbol('234')
+const anotherId = Symbol('234')
+
+// console.log(id === anotherId);
+
+let bigNumber = 312344231321n
+// console.log(typeof bigNumber);
+
+const heros = ["hulk" , "vision" , "messi"]
+
+let Obj = {
+    name:"vision",
+    age:100,
+    writing: "right-handed",
+}
+
+const myFunction = function(){ // type is function/function-object
+    console.log("hello world");
+}
+
+console.log(typeof outsideTemp);
+
