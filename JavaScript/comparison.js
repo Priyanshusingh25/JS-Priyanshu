b3f0f4d6a7198ea60b43d1ccc4b080092307c8f4
PRIYANSHU SINGH <92785258+Priyanshusingh25@users.noreply.github.com>
2024-07-31 06:35:48 +0000

    comparisons js file

---
+++ JavaScript/comparison.js
@@ -0,0 +1,13 @@
+//greater than, equal to, not equal to
+
+// console.log("2" > 1)
+
+// console.log(null > 1)
+// console.log(null == 1)
+// console.log(null >= 0)
+// console.log(null <= 1) //null hold greater value than 0 ; undefined gives false values
+
+/// ===
+
+console.log("2" == 2);
+console.log("2" === 2); // checks if it is string
