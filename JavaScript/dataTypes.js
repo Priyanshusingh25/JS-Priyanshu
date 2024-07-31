eab86ac5b75e436ef89a50e498313ca0b7bdf8c9
PRIYANSHU SINGH <92785258+Priyanshusingh25@users.noreply.github.com>
2024-07-31 05:17:01 +0000

    datatypes

---
+++ JavaScript/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //preserving older code, code is treated as newer version
+
+// alert("hello"); (node.js => Browser)
+console.log(3+3) 
+console.log("priyanshu")  //code on next line, as readability is preffered 
+
+let name="priyanshu"
+let age="18"
+let isLoggedIn= false
+let state = null // shows emptiness , type is object
+
+          // primitive datatypes
+// number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => true or false
+//null -- standalone value type
+//undefined
+//symbol -- for uniqueness
+
+console.log(typeof age);
